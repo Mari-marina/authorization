78f2691d90639808048853eb197a19ba6a35c1d9
Mari-marina <83422160+Mari-marina@users.noreply.github.com>
2023-01-28 18:14:11 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,27 @@ function test(a, b) {
     return a + b
 }
 
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
 
+
+const number = document.querySelector('.text-righ');
+const email = document.querySelector('.text-email');
+
+const secondBlock = document.querySelector('.authorization_second-block');
+const firstBlock = document.querySelector('.authorization_first-block');
+
+
+number.addEventListener('click', function () {
+  secondBlock.classList.toggle('second-block-active');
+});
+number.addEventListener('click', function () {
+  firstBlock.classList.toggle('first-block-active');
+});
+
+
+email.addEventListener('click', function () {
+  firstBlock.classList.toggle('first-block-inactive');
+});
+email.addEventListener('click', function () {
+  secondBlock.classList.toggle('first-block-active');
+});
 //# sourceMappingURL=script.js.map
